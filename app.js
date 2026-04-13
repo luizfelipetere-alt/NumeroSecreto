3f473c5be860385237ee78e064752116e3cf740a
luizfelipetere-alt <luizfelipe.tere@gmail.com>
2026-04-13 16:42:03 -0300

    Revert "Alteração de 100 para 50."
    This reverts commit e41748b9b7deaa1929f3380a4b49e783189bce15.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 25;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -13,7 +13,7 @@ function exibirTextoNaTela(tag, texto){
 mensagemInicial()
 function mensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 25');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 function verificarChute(){
